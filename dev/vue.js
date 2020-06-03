81f21b2188d0d28173e6dde838aeb4c3f1d4f60c
jdlrobson <jdlrobson@gmail.com>
2020-06-02 20:51:14 -0700

    Deploy production

--- dev/vue.js
+++ dev/vue.js
@@ -1,3 +1,5 @@
-const Vue = require( '~/../node_modules/vue/dist/vue.common.dev.js' );
+const Vue = process.env.NODE_ENV === 'development' ?
+    require( '~/../node_modules/vue/dist/vue.common.dev.js' ) :
+    require( '~/../node_modules/vue/dist/vue.common.prod.js' );
 
 module.exports = Vue;
